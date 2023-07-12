8bd320837e42c1fbe622a07eb803a5bf89f8dcd2
KASETTY MAYURI <k.mayuri49@gmail.com>
2023-07-12 15:59:32 +0530

    successfully uploaded project

---
+++ Frontend/ecommerce/src/Components/JS/Log.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import  { useState } from 'react';
+import '../CSS/Log.css';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import loginContextLogin from '../../createContext/createContextLogin';
+const Log = () => {
+
+  const state=useContext(loginContextLogin);
+    const [user,setUser]=useState('');
+    const [pass,setPass]=useState('');
+    const navigate=useNavigate();
+
+      const onSubmit = (e) => {
+        e.preventDefault();
+        
+        let localLogin=JSON.parse(localStorage.getItem('login'));
+        if(user==""|| pass==""){
+          alert('Enter the credentials')
+          return
+        }
+        else if(localLogin==null) 
+        {
+          alert('Signup First');
+          return
+        }
+        if(localLogin.username==user && localLogin.password==pass)
+        {
+            navigate('/');
+            state.toggleFlag();
+        }
+        else{
+          alert("Wrong Credentials")
+        }
+       
+
+      }
+    
+  return (
+    <div className='log'>
+      <div className='login_form'>
+      <h1 className='h1_heading_login'>Welcome to login page...</h1>
+        <input onChange={(e)=>setUser(e.target.value)} type="text" label='username' placeholder='Username' required />
+        <input onChange={(e)=>setPass(e.target.value)} type="password" label='username' placeholder='Password' required />
+        <button className="sub_btn" onClick={onSubmit}>Submit</button>
+     
+      
+      </div>
+    </div>
+  )
+}
+
+export default Log;
